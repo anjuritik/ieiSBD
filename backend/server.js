5343b7a1a6c5a9d6d3548505ce0c48957233f192
Anjana Nayak <anjuritik@gmail.com>
2025-07-19 18:56:37 +0530

    env -added

--- backend/server.js
+++ backend/server.js
@@ -19,8 +19,13 @@ app.get('/', (req, res) => {
   res.send('API is running 🚀');
 });
 
-const port = process.env.PORT || 3000||5000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(process.env.PORT || 3000, '0.0.0.0', () => {
+  console.log(`Server running on port ${process.env.PORT || 3000}`);
+});
+
+
+/* const port = process.env.PORT || 3000||5000;
+app.listen(port, () => console.log(`Server running on port ${port}`)); */
 /* app.listen(5000, () => {
   console.log('✅ Server running on http://localhost:5000');
 });
